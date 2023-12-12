75fda1bbb63acc5f71a01196fb1fe7ba39902364
Bill Cathey <billcathey@hotmail.com>
2023-12-12 13:28:40 -0500

    Create route to get all words

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import Word from './models/wordModel.js'
 
 dotenv.config()
 
@@ -9,6 +10,20 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => res.send('API running'))
+app.get('/words', async (req, res) => {
+    try {
+        const words = await Word.find({})
+        return res.status(200).json({
+            count: words.length,
+            data: words
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send({message: error.message})
+    }
+})
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
