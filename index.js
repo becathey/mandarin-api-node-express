c427b3a6b38ee54d7a680bba56257a055cfd3716
Bill Cathey <billcathey@hotmail.com>
2023-12-12 20:58:04 -0500

    Add CORS middleware

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import connectDB from './config/db.js'
-// import Word from './models/wordModel.js'
 import wordRoutes from './routes/wordRoutes.js'
 
 dotenv.config()
@@ -12,6 +12,13 @@ connectDB()
 const app = express()
 
 app.use(express.json())
+app.use(
+    cors({
+        origin: 'https://mandarin-english.netlify.app',
+        methods: ['GET'],
+        allowedHeaders: ['Content-Type'],
+    })
+)
 
 app.get('/', (req, res) => res.send('API running'))
 app.use('/words', wordRoutes)
